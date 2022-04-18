025b72d216b4a4233ef737c92ddeab11e139c859
loic quinquenel <loic.qql@gmail.com>
2022-04-18 03:38:43 +0200

    added uiavatar

---
+++ components/ui/UiAvatar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="ui-avatar" :style="color">
+      <p class="ui-avatar__letter">{{letter}}</p>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }, 
+  computed: {
+    letter() {
+      return this.name.charAt(0).toUpperCase();
+    },
+    color() {
+      let colors = ['#c7a21c', '#04d8d1'];
+      return {backgroundColor : colors[Math.floor(Math.random() * colors.length)]}
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.ui-avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  &__letter {
+    width: 100%;
+    height: 100%;
+    @include d-flex-center;
+    font-family: $secondary-font;
+    font-weight: $semi-bold-font-weight;
+    color: #fff;
+  }
+}
+</style>
