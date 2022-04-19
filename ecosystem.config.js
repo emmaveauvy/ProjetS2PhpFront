cdc7416648102ee10d3da24b9281b57e28da6349
loic quinquenel <loic.qql@gmail.com>
2022-04-19 19:30:20 +0200

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Cami-Front',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
